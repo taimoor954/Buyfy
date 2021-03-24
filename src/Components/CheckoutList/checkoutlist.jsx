a3e55ef4392b1d4d592bbb58d3c29781b02bc19c
taimoor954 <taimoormuhammad954@gmail.com>
2021-03-25 00:33:33 +0500

    checkout list static structure

--- src/Components/CheckoutList/checkoutlist.jsx
+++ src/Components/CheckoutList/checkoutlist.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import Checkoutlistitems from '../CheckoutListItems/checkoutlistitems';
+import Paragraph from '../Paragraphs/paragraphs';
 import "./checkoutlist.css"
 const CheckoutList = ({cartItems}) => {
     console.log(cartItems)
     return (
         <div className='checkoutList'>
+            <div className='checkout-list-item checkout-list-item-extend' >
+                <Paragraph fontSize={28} fontWeight='bold'>Product</Paragraph>
+                <Paragraph fontSize={28} fontWeight='bold'>Quantity</Paragraph>
+                <Paragraph fontSize={28} fontWeight='bold'>Price</Paragraph>
+            </div>
             {cartItems.map((item)=> <Checkoutlistitems key={item.id} {...item} />)}
         </div> 
     )
--- src/Components/CheckoutListItems/checkoutlistitems.jsx
+++ src/Components/CheckoutListItems/checkoutlistitems.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import Paragraph from "../../Components/Paragraphs/paragraphs.jsx"
+import Button from "../../Components/Buttons/Button.jsx"
 // import Paragraph from "../Paragraphs"
 import { addProductToCart, removalProductFromCartAction, deleteProductFromCartAction } from '../../Redux/Cart/cartActions';
 import "./checkoutlistitems.css"
@@ -12,11 +13,19 @@ const Checkoutlistitems = ({deleteProductFromCartAction,removalProductFromCartAc
             <div className="checkout-item-product-image"></div>
             <Paragraph fontSize={24} fontWeight='semi-bold'>My product</Paragraph>                
         </div>    
-        <div className='checkout-item-quantity'></div>    
-        <div className='checkout-item-price'></div>    
-        <div className='checkout-item-cross'></div>    
+        <div className='checkout-item-quantity center' style={{flexFlow:'row'}}>
+            <Button fontWeight='bold' color='black' background='white' additionalStyle={{borderTopLeftRadius :"20px", borderBottomLeftRadius:'20px' , transform:'translateX(5px)'}} >+</Button>
+            <Button fontWeight='bold' color='black' background='white' additionalStyle={{borderRadius :"0px"}}>3</Button>  
+            <Button fontWeight='bold' color='black' background='white' additionalStyle={{borderTopRightRadius :"20px", borderBottomRightRadius:'20px', transform:'translateX(-5px)'}}>-</Button>
+        </div>    
+        <div className='checkout-item-price center'>
+            <Paragraph fontWeight='semi-bold' fontSize={20}>$240</Paragraph>
+            </div>    
+        <div className='checkout-item-cross center'>
+        <Paragraph AdditionalStyle={{cursor:"pointer"}} fontWeight='bold' fontSize={30}>X</Paragraph>
+            </div>    
 
-    
+
             {/* <h1>{title} - {cost} <button onClick={()=>deleteProductFromCartAction(id)}>x</button></h1>
            <h3><button onClick={()=>addProductToCart(product)}>+</button> {quantity} <button onClick={()=>removalProductFromCartAction(id)}>-</button></h3> 
      */}

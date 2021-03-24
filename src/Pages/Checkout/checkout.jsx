0dde531309cadf63964cc5199c5a2474ef7500e5
taimoor954 <taimoormuhammad954@gmail.com>
2021-03-25 01:39:53 +0500

    done with checkout styling

--- src/Pages/Checkout/checkout.jsx
+++ src/Pages/Checkout/checkout.jsx
@@ -2,8 +2,10 @@ import React, {useState} from "react";
 import CheckoutList from "../../Components/CheckoutList/checkoutlist";
 import { connect } from "react-redux";
 import { orderTotalCalculation } from "../../Utility/checkout";
+import Header from '../../Components/Header/header.jsx' 
 import OrderForm from '../../Components/OrderForm/OrderForm';
 import './checkout.css'
+import Button from "../../Components/Buttons/Button";
 
 const Checkout = ({total}) => {
     var [OrderFormDisplay, setOrderFormDisplay] = useState(false)
@@ -11,9 +13,14 @@ const Checkout = ({total}) => {
     return (
     <div className='checkout-page-container'>
       <div className='checkout'>
-      <h1>Checkout page</h1>
+      <Header style={{letterSpacing:'4px'}} fontSize={60} fontWeight='bold'>Shopping Cart</Header>
       <CheckoutList />
-      <h1>Total Amount - {total}</h1>
+      <div className='checkoutBottomContent'>
+      <Header style={{letterSpacing:'4px'}} fontSize={60} fontWeight='bold'>Total: $323</Header>
+      <Button additionalStyle={{justifySelf:'end', borderRadius:'20px'}} fontSize={28} fontWeight='bold' background='white' color='black'>Proceed And Pay</Button> 
+
+      </div>
+      {/* <h1>Total Amount - {total}</h1> */}
      {/* {OrderFormDisplay && <OrderForm/>}  
  <button onClick={(e) => setOrderFormDisplay(!OrderFormDisplay)}>Proceed & Pay {total}</button>  */}
 

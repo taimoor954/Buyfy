aa9987d0cfb09894efd910a1ebac8f91fc0cae6e
taimoor954 <taimoormuhammad954@gmail.com>
2021-03-28 17:44:05 +0500

    implemented Material UI on sign in form. Done with sign in form

--- src/Components/SigninForm/signin.jsx
+++ src/Components/SigninForm/signin.jsx
@@ -1,39 +1,53 @@
-import React,{useState} from 'react'
-import {connect} from 'react-redux'
-import {signin} from '../../Redux/Authentication/authActions';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { signin } from '../../Redux/Authentication/authActions';
 import TextField from '@material-ui/core/TextField';
 import Button from '../Buttons/Button';
+import './signin.css';
+const Signin = ({ signin }) => {
+  var [email, setEmail] = useState('');
+  var [password, setPassword] = useState('');
+  var handleFormSumit = (e) => {
+    e.preventDefault();
+    var credentials = {
+      email,
+      password,
+    };
+    signin(credentials);
+  };
 
-const Signin = ({signin}) => {
-    var [email, setEmail] = useState('')
-    var [password, setPassword] = useState('')
-    var handleFormSumit =(e) => {
-        e.preventDefault()
-        var credentials  = {
-            email,
-            password
-        }
-        signin(credentials)
-    }
+  return (
+    <form className="signinForm" onSubmit={handleFormSumit}>
+      <TextField
+        id="standard-basic"
+        style={{ width: '100%', marginTop: '2em' }}
+        label="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+      />
+      <TextField
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        id="standard-basic"
+        style={{ width: '100%', marginTop: '2em' }}
+        label="Password"
+      />
+      <Button
+        background="rgba(0,0,0,0.5)"
+        additionalStyle={{ width: '60%', height: '40px' }}
+        color="white"
+      >
+    Sign In
+    </Button>
 
-
-    return (
-        <div>
-            <h1>Sign In</h1>
-            <form onSubmit={handleFormSumit}>
-            <TextField id="standard-basic" style={{width:"100%"}} label="Standard" />
-            <TextField id="standard-basic" style={{width:"100%"}} label="Standard" />
-            <Button background='rgba(0,0,0,0.5)' additionalStyle={{width:'70%', height:'30%'}}/>
-
-            {/* <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type='text' placeholder='email'></input>
+      {/* <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type='text' placeholder='email'></input>
             <input onChange={(e)=>{setPassword(e.target.value)}} value={password} type='text' placeholder='password'></input>
             <button type='submit'>Submit</button> */}
-            </form>
-        </div>
-    )
-}
+    </form>
+  );
+};
 var actions = {
-    signin
-}
+  signin,
+};
 
-export default connect(null,actions)(Signin)
+export default connect(null, actions)(Signin);

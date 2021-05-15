e57e32109e53c73b681b0f89e61c45a33263e431
taimoor954 <taimoormuhammad954@gmail.com>
2021-05-15 21:20:26 +0500

    firestore rules error not solved yet

--- src/Redux/Orders/orderActions.js
+++ src/Redux/Orders/orderActions.js
@@ -1,21 +1,21 @@
-import { firestore, storage } from "../../Firebase/firebase";
-import { serverTimestamp } from "../../Firebase/firebase";
+import { firestore, storage } from '../../Firebase/firebase';
+import { serverTimestamp } from '../../Firebase/firebase';
 import history from '../../History/history';
 
 export const generateOrder = () => async (dispatch, getState) => {
-try { 
-    var {cart:products, authentication} = getState()
+  try {
+    var { cart: products, authentication } = getState();
     var orderInfo = {
-        ...authentication,
-        products,
-        orderAt : serverTimestamp,
-        orderStatus : 'pending'
-    }
-    var order = await firestore.collection('Orders').add(orderInfo)
-    console.log(order.id)
-    console.log(history)
-    history.push(`/checkout/${order.id}`) //redirecting from action 
-} catch (error) {
-    console.log(error)
-}
-}
+      ...authentication,
+      products,
+      orderAt: serverTimestamp,
+      orderStatus: 'pending',
+    };
+    var order = await firestore.collection('Orders').add(orderInfo);
+    console.log(order.id);
+    console.log(history);
+    history.push(`/checkout/${order.id}`); //redirecting from action
+  } catch (error) {
+    console.log(error);
+  }
+};
